a5868be9524085b2bce6ee23a25f58e6c7e45021
Feiyang <feiyang.f.li@gmail.com>
2025-08-20 15:32:20 -0400

    minor change by adding dayOfWeekList

--- calendar-react/calendar/src/mainCalendar/weekCal.jsx
+++ calendar-react/calendar/src/mainCalendar/weekCal.jsx
@@ -1,11 +1,11 @@
 import styles from "./weekCal.module.css";
 import classNames from "classnames";
-import {today} from "../shared/scripts/date.js"
-import { useEffect} from 'react'
-function DayOfWeek({date, week}) {
+import {generateWeekDays} from "../shared/scripts/date.js"
+import { useEffect, useState} from 'react'
+function DayOfWeek({date, week, idx}) {
     // return a buttom with 
     const rep = {0: "Sun", 1: "Mon", 2: "Tue", 3: "Wed", 4: "Thu", 5: "Fri", 6: "Sat"};
-    <li className={styles.weekCalendar__dayofWeek__item}>
+    <li className={styles.weekCalendar__dayofWeek__item} key={idx}>
         <button className={styles.weekCalendar__dayOfWeek__button}>
             <span className={styles.weekCalendar__dayOfWeek__day}>{rep[week]}</span>
             <span className={styles.weekCalendar__dayOfWeek__date}>{date}</span>
@@ -17,15 +17,15 @@ export default function WeekCal({ storage, selectedDate, setSelectedDate }) {
     const clocks = ["12:00 AM", "1:00 AM", "2:00 AM", "3:00 AM", "4:00 AM", "5:00 AM", "6:00 AM", "7:00 AM", "8:00 AM", "9:00 AM", 
                     "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM", "7:00 PM",
                     "8:00 PM", "9:00 PM", "10:00 PM", "11:00 PM"];
-                    
+    const [dayOfWeeks, setDayOfWeeks] = useState(generateWeekDays(selectedDate));
     useEffect(() => {
-
-    }, [])
+        setDayOfWeeks(generateWeekDays(selectedDate));
+    }, [selectedDate]);
     return (
     <>
       <div className={styles.weekCalendar}>
         <ul className={styles.weekCalendar__dayOfWeekList}>
-
+            {dayOfWeeks.map((ele, idx) => (<p>ha</p>))}
 
         </ul>
         <ul className={styles.weekCalendar__allDayList}>
